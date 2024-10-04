2f63c497eda884bf8b98bdaab9572e59556a4e15
dilipMaharjan <dilipmaharjann@gmail.com>
2024-10-03 19:09:29 -0500

    gitignore virtual env

--- docker/building_docker_images/app.py
+++ docker/building_docker_images/app.py
@@ -1,3 +1,11 @@
 import os
-message=os.environ.get('MESSAGE')
-print(f'Hello form Docker Image, {message} !')
+import sys
+import requests
+
+message = os.environ.get("MESSAGE")
+print(f"Hello form Docker Image new, {message} !")
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+
+print (sys.version)
